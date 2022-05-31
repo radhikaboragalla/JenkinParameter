9a18f555d50064621359e67abe6a4586970fc902
CSC <CSC@DESKTOP-C5HQ12V>
2022-05-31 00:10:49 -0700

    Jenkins with parameter first commit

---
+++ src/test/java/com/samplejenkinparameter/JenkinParameterTest.java
@@ -0,0 +1,43 @@
+package com.samplejenkinparameter;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class JenkinParameterTest {
+	
+	public  WebDriver driver=null;
+	
+	@Test
+	public  void jenkinParameterTest()
+		
+	{
+		String browser=System.getProperty("browser");
+		
+		String url=System.getProperty("url");
+		switch(browser) {
+		 case "chrome":
+			 WebDriverManager.chromedriver().setup();
+			 driver = new ChromeDriver();
+			 break;
+		 case "firefox":
+			 WebDriverManager.firefoxdriver().setup();
+			 driver = new FirefoxDriver();
+			 break;
+			 }
+		driver.get(url);
+		driver.quit();
+			
+		     
+		
+		}
+	}
+	
+	
+
+
